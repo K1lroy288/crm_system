c486981a05276c1c755032c3e14810560d3eb96b
K1lroy <k1lroyd@mail.ru>
2025-12-27 22:05:26 +0300

    create api dateway

--- user_service/model/user.go
+++ user_service/model/user.go
@@ -12,7 +12,7 @@ type User struct {
 	Username     string         `gorm:"size:100;not null;unique"`
 	FirstName    string         `gorm:"size:50"`
 	LastName     string         `gorm:"size:50"`
-	PasswordHash []byte         `gorm:"size:255;not null"`
+	PasswordHash []byte         `gorm:"not null"`
 	CreatedAt    time.Time      `gorm:"autoCreateTime"`
 	UpdatedAt    time.Time      `gorm:"autoUpdateTime"`
 	DeletedAt    gorm.DeletedAt `gorm:"index"`
