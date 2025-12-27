76cdd663f02c2c33d02dc179f7610bb1462cd409
K1lroy <k1lroyd@mail.ru>
2025-12-27 21:17:55 +0300

    add indexes in users table

--- user_service/db/migrations/001_create_users.up.sql
+++ user_service/db/migrations/001_create_users.up.sql
@@ -8,3 +8,6 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at TIMESTAMPTZ DEFAULT NOW(),
     deleted_at TIMESTAMPTZ
 );
+
+CREATE INDEX IF NOT EXISTS idx_users_username ON users(username);
+CREATE INDEX IF NOT EXISTS idx_users_last_name ON users(last_name);
